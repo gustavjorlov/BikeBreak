e435c008d263771862f9b1b1aa1a8eade86e10ad
Gustav Jorlöv <gustav.jorlov@kits.se>
2016-03-28 23:31:45 +0200

    some dummy db setup

--- server/src/persist.js
+++ server/src/persist.js
@@ -1,7 +1,20 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017/bikebreak';
 
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(url, (err, db) => {
     console.log("Connected correctly to server.", err, db);
-    db.close();
+    insertDocument(db, () => {
+        db.close();
+    });
 });
+
+
+const insertDocument = (db, callback) => {
+    db.collection('exercises').insertOne({
+        "date": new Date(),
+        "stuff": "things..."
+    }, (err, result) => {
+        console.log("Inserted a document into the restaurants collection.", err, result);
+        callback();
+    });
+};
--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,19 @@
 import express from 'express';
 import {getExcersize} from './filereader';
+import * as db from './persist.js';
 
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("hello world");
+});
+
+app.get("/exercises", (req, res) => {
+    db.getAllTheStuff().then((stuff) => {
+        res.json(stuff);
+    }, {
+        res.sendStatus(500);
+    });
 });
 
 
