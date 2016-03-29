63db88aaf788c51f861b9bab9e59c7c7f788a480
Gustav Jorlöv <gustav.jorlov@kits.se>
2016-03-29 08:57:20 +0200

    implemented dummy stuff for file upload. Though there is a chance I don't need it

--- server/src/persist.js
+++ server/src/persist.js
@@ -3,17 +3,12 @@ const url = 'mongodb://localhost:27017/bikebreak';
 
 MongoClient.connect(url, (err, db) => {
     console.log("Connected correctly to server.", err, db);
-    insertDocument(db, () => {
-        db.close();
-    });
+    insertExercise.bind(null, db);
 });
 
 
-const insertDocument = (db, callback) => {
-    db.collection('exercises').insertOne({
-        "date": new Date(),
-        "stuff": "things..."
-    }, (err, result) => {
+export const insertExercise = (db, document, callback) => {
+    db.collection('exercises').insertOne(document, (err, result) => {
         console.log("Inserted a document into the restaurants collection.", err, result);
         callback();
     });
--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,31 @@
 import express from 'express';
 import {getExcersize} from './filereader';
-import * as db from './persist.js';
+import {insertExercise} from './persist.js';
+import busboy from 'connect-busboy';
 
 const app = express();
+app.use(busboy());
+
+app.use(function(req, res) {
+    req.busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+        console.log("Got a file", fieldname, file, filename, encoding, mimetype);
+    });
+});
 
 app.get("/", (req, res) => {
     res.send("hello world");
 });
 
+app.post("/exercise", (req, res) => {
+    // upload file
+    // parse file
+    // store exercise in database
+});
+
 app.get("/exercises", (req, res) => {
     db.getAllTheStuff().then((stuff) => {
         res.json(stuff);
-    }, {
+    }, (err) => {
         res.sendStatus(500);
     });
 });
