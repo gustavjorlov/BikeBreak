5fbf786b78eb2851b0f26e094c1e5d6c5369da97
Gustav Jorlöv <gustav.jorlov@kits.se>
2016-03-29 09:03:14 +0200

    added some dummy graphical elements to the client

--- client/src/app.js
+++ client/src/app.js
@@ -1,17 +1,22 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import Header from './components/Header.js';
 import FileUpload from './components/FileUpload.js';
+import ExerciseList from './components/ExerciseList.js';
+import Footer from './components/Footer.js';
 
 class Application extends React.Component{
     render(){
         return (
             <div className="application">
+                <Header />
                 <FileUpload />
+                <ExerciseList />
+                <Footer />
             </div>
         );
     }
 }
-
 
 ReactDOM.render(
     <Application />,
